466e4f3bf96198cc5ea77f8e1e5e34aaaaa3be43
khoerulmutaqin225 <khoerulmutaqin225@gmail.com>
2024-07-08 09:31:59 +0700

    replace Shop with Reservasi

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,18 @@
- UPDATE pos_config
- SET "name" = 'Reservasi'
- WHERE "name" = 'Shop'
- RETURNING id, "name"
+-- Odoo data.sql format
+UPDATE pos_config
+SET name = 'Reservasi'
+WHERE name = 'Shop';
+
+UPDATE pos_config
+SET start_category = 'False'
+WHERE name = 'Reservasi';
+
+
+UPDATE pos_config
+SET limit_categories = 'True'
+WHERE name = 'Reservasi';
+
+INSERT INTO pos_category_pos_config_rel (pos_config_id, pos_category_id)
+SELECT 1, pc.id
+FROM pos_category pc
+WHERE pc.name IN ('Facilities rent', 'Jetty and Mat. Handling', 'Other Support Service');
